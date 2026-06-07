dc17715b8aa3655a20ceec293770c1aa7f76f664
Rtm678 <rpmechanick@gmail.com>
2026-06-07 23:42:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,10 +41,6 @@ db.run(`
     )
 `);
 
-
-});
-
-
 const WEAK_PASSWORDS = new Set([
     '123','1234','12345','123456','1234567','12345678','123456789','1234567890',
     'qwerty','qwerty123','password','password1','pass','passwd',
